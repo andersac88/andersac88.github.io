7eb58b56fdb3e6fe32a5ab950fb8fffe54aa8527
Andrew Andersen <andersac88@gmail.com>
2018-10-29 18:20:03 -0500

    reload

---
+++ app.js
@@ -0,0 +1,31 @@
+$(".hangApp").on('click', function(){
+    window.location = "https://andersac88.github.io/Word-Guess-Game/";    
+});
+
+$(".hangCode").on('click', function(){
+    window.location = "https://github.com/andersac88/Word-Guess-Game";    
+});
+
+$(".triviaCode").on('click', function(){
+    window.location = "https://github.com/andersac88/TriviaGame"
+});
+
+$(".triviaApp").on('click', function(){
+    window.location = "https://andersac88.github.io/TriviaGame/"
+})
+
+$(".gifCode").on('click', function(){
+    window.location = "https://github.com/andersac88/GifTastic/"
+});
+
+$(".gifApp").on('click', function(){
+    window.location = "https://andersac88.github.io/GifTastic/"
+})
+
+$(".trainCode").on('click', function(){
+    window.location = "https://github.com/andersac88/Train-Scheduler/"
+});
+
+$(".trainApp").on('click', function(){
+    window.location = "https://andersac88.github.io/Train-Scheduler/"
+})
