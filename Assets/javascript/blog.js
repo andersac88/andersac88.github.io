d26942406be748feb4455731dad893f9bd2d5689
Andrew Andersen <andersac88@gmail.com>
2020-06-24 21:30:48 -0500

    blog updates

--- Assets/javascript/blog.js
+++ Assets/javascript/blog.js
@@ -1,6 +1,9 @@
 let blogPosts = [{
     Date: "June 18, 2020",
-    Text: "Test"
+    Text: "This is my first attempt at a blog post and I am not sure what to say however with everything going on in the world "
+}, {
+    Date: "June 18, 2020",
+    Text: "This is my first attempt at a blog post and I am not sure what to say however with everything going on in the world "
 }]
 
 function blogDrop() {
@@ -13,6 +16,20 @@ function blogDrop() {
         a.attr("data-text", `${blogPosts[i].Details}`)
         $(".blog").append(a)
 }
+
+}
+
+function renderBlog() {
+    for (let j = 0; j < blogPosts.length; j++) {
+        let entry = $(`<div class="row my-5">
+                            <div class="col-4">${blogPosts[j].Date}</div>
+                            <div class="col-8">${blogPosts[j].Text}</div>
+                        </div>`)
+        $(".blogs").append(entry);
+        
+    }
+
 }
 
 blogDrop();
+renderBlog();
